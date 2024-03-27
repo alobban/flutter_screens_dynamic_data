e03f6cbac99b925a1f867d84ad11c15f655e65b1
Andrew Lobban <andrewglobban@gmail.com>
2024-03-27 12:28:04 -0500

    Add Transition between Meals and Meal Detail screens with Hero widget

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 import 'package:meals/widgets/meal_item_trait.dart';
-import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
 
 class MealItem extends StatelessWidget {
@@ -37,12 +37,15 @@ class MealItem extends StatelessWidget {
         onTap: onSelectMeal,
         child: Stack(
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              fit: BoxFit.cover,
-              height: 200,
-              width: double.infinity,
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+              ),
             ),
             Positioned(
               bottom: 0,
